261ef11c8e0c3d2269682ff457fdd0d1d16e9f80
unknown <dwu@tutanota.com>
2021-08-26 21:21:31 +1000

    Added Bash to support win,mac,linux,freebsd

--- install.sh
+++ install.sh
@@ -3,14 +3,33 @@ echo "========================================================="
 echo "==!! Simpletrons : Simplify Opentrons API 1 for OT-1!!=="
 echo "========================================================="
 echo "============="
-#Detect Ubuntu or Window Version
-
-
-#Ubuntu Install 
-sudo apt update && upgrade -y
-sudo apt install python git -y
-sudo python3 pip install opentrons==2.5.2
-sudo python3 pip install curse
-sudo python3 pip install mysql-connector-python 
-
-#Window Bash/Install
+#Detect Ubuntu or Window Version or Max OS
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sudo apt update && upgrade -y
+		sudo apt install python git -y
+		sudo python3 pip install opentrons==2.5.2
+		sudo python3 pip install curse
+		sudo python3 pip install mysql-connector-python 
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        pip install opentrons==2.5.2
+        pip install cursed
+        pip install mysql-connector-python 
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        pip install opentrons==2.5.2
+        pip install windows-curses
+        pip install mysql-connector-python 
+elif [[ "$OSTYPE" == "msys" ]]; then
+        pip install opentrons==2.5.2
+        pip install windows-curses
+        pip install mysql-connector-python 
+elif [[ "$OSTYPE" == "win32" ]]; then
+        pip install opentrons==2.5.2
+        pip install windows-curses
+        pip install mysql-connector-python 
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        pkg apt update && upgrade -y
+        pkg install python3.7 git -y
+		python3 pip install opentrons==2.5.2
+		python3 pip install curse
+		python3 pip install mysql-connector-python 
+else
